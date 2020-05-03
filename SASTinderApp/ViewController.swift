4667203d26c0e6453dec845a2b120c07e29a8c61
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-05-03 10:08:07 +0530

    Initial Commit

---
+++ SASTinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SASTinderApp
+//
+//  Created by Sukumar Anup Sukumaran on 03/05/20.
+//  Copyright © 2020 Tech_Tonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
